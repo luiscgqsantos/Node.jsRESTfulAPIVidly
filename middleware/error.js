a671c21c886af48ca51ff3fbde0f03cf4f7e2a98
Luís Santos <1170033@isep.ipp.pt>
2019-11-25 17:25:09 +0000

    Handling and Logging Errors P2

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,7 @@
 const winston = require('winston');
 
 module.exports = function (err, req, res, next) {
-    winston.log('error', err.message);
+    winston.error(err.message, err);
 
     res.status(500).send('Something failed.');
 };
