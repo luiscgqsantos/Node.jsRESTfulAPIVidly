08c4ac5fe0d346907400fc4129bdc2fac70a9b89
Luís Santos <luis.santos@mindera.com>
2020-01-29 15:44:41 +0000

    users fix

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,39 @@
-const auth = require('../middleware/auth');
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const _ = require('lodash')
-const express = require('express');
-const mongoose = require('mongoose');
+const auth = require("../middleware/auth");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const {
-    User,
-    validate
-} = require('../models/user');
+const { User, validate } = require("../models/user");
 
-router.get('/me', auth, async (req, res) => {
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
 
-    const user = await User.findById(req.user._id).select('-password');
-
-    res.send(user);
+  res.send(user);
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const {
-        error
-    } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  let user = await User.findOne({
+    email: req.body.email
+  });
+  if (user) return res.status(400).send("User already registered.");
 
-    let user = await User.findOne({
-        email: req.body.email
-    });
-    if (user) return res.status(400).send('User already registered.');
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
-    user = new User(
-        _.pick(req.body, ['name', 'email', 'password'])
-    );
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
 
-    await user.save();
+  const token = user.generateAuthToken();
 
-    const token = user.generateAuthToken();
-
-    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
-
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
